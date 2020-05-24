1fc0151523484b736414c988c7abf5e94dabf5cb
Ethan Benjamin <ethanb3njamin@gmail.comn>
2020-05-23 21:29:54 -0400

    push Covid-19 Tracker code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import {Cards, Chart, CountryPicker} from './components';
+import styles from './App.module.css';
+
+// index files; can use the name of the function to import
+import {fetchData} from "./api";
+
+
+class App extends React.Component {
+    state = {
+        data: {},
+        country: '',
+    }
+    // after the component is mounted, makes changes to the state
+  async componentDidMount(){
+        const fetchedData = await fetchData();
+
+        // setting the state of the date when the component is rendered
+        this.setState({data: fetchedData});
+    }
+
+    handleCountryChange = async (country) => {
+        const data = await fetchData(country);
+
+        this.setState({data, country: country});
+        // fetch data and set the state
+    }
+    render(){
+        // taking the data out the state
+        const {data, country} = this.state;
+
+        return (
+            // styles.container is making sure the module is used in css
+            <div className={styles.container}>
+                <img className={styles.image} alt="logo" src={"https://www.diagnosticsolutionslab.com/sites/default/files/COVID19-NP-Logo.png"} />
+              <Cards data={data} />
+              <CountryPicker handleCountryChange={this.handleCountryChange} />
+              <Chart data={data} country={country}/>
+            </div>
+        )
+    }
 }
 
 export default App;
