f2dfabc9a738b4d058cdb30c0e68ec976b26e1c7
Ethan Benjamin <ethanb3njamin@gmail.comn>
2020-05-23 21:29:06 -0400

    Updates

---
+++ precache-manifest.b0b8bdafd012992f4887a89c92c030f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddfe785ab3a5a29c6021bb6857ac767",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "23178f72922ddf144dd6",
+    "url": "/covid/static/css/main.99056cde.chunk.css"
+  },
+  {
+    "revision": "6e341679535948e838db",
+    "url": "/covid/static/js/2.1fe112ba.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.1fe112ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23178f72922ddf144dd6",
+    "url": "/covid/static/js/main.11804f6b.chunk.js"
+  },
+  {
+    "revision": "8affece6a1a33bdd2b00",
+    "url": "/covid/static/js/runtime-main.a96d790a.js"
+  }
+]);
